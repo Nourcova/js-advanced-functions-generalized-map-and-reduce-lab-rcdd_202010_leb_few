3024b91af1895799f21f2646c2cab0d0051aa601
Ibrahim Nour <ibrahamnour22@gmail.com>
2021-02-22 14:21:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-// Add your functions here
-// function map(src, cb) {
-//   let r = []
+Add your functions here
+function map(src, cb) {
+  let r = []
 
-//   for (let i = 0; i < src.length; i++) {
-//     let theElement = src[i]
-//     r.push(cb(theElement))
-//   }
+  for (let i = 0; i < src.length; i++) {
+    let theElement = src[i]
+    r.push(cb(theElement))
+  }
 
-//   return r;
-// }
+  return r;
+}
 
-// function reduce(src,cb){
+function reduce(src,cb){
   
-// }
+}
 let a=[1,2,3,4]
 function mapToNegativize(arr){
 const mapped = arr.map(elem =>elem*-1);
